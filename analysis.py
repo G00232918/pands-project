880d4bafb63ace0a1f2e5b7fa666c831d63f419b
james <g00232918@gmit.ie>
2022-04-10 15:28:40 +0100

    update

--- analysis.py
+++ analysis.py
@@ -7,26 +7,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+# import csv file for pandas
 iris_data = pd.read_csv("iris_data.csv")
-
-# https://realpython.com/python-csv/
-# https://stackoverflow.com/questions/43801152/read-output-of-a-csv-file-and-write-it-to-a-text-file-using-python
-
-
-import csv
-
-file = open("iris.txt", "w")
-with open("iris_data.csv") as csvFile:
-    # call the CSV import to read the CSV file
-    readCSV = csv.read(csvFile)
-    for row in readCSV:
-        Id = row[0]
-        Id1 = row[1]
-        file.write()
-file.close()
-
-
 
 # use the head function to ensure you have the correct dataframe
 print("Showing the first 5 lines\n")
