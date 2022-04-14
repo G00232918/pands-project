750953edb0c725a390dc566d2bfbcc7d1c3f6deb
james <g00232918@gmit.ie>
2022-04-14 13:05:46 +0100

    updates

--- analysis.py
+++ analysis.py
@@ -5,7 +5,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # import csv file for pandas
 iris_data = pd.read_csv("iris_data.csv")
@@ -29,7 +28,7 @@ print(iris_data.info())
 
 # Use the describe method to analyse the data where the values can be
 # used further on in the program.
-# https://realpython.com/python-csv/
+# Reference - https://realpython.com/python-csv/
 print("\n-----Analysis of the data-----")
 print(iris_data.describe())
 with open("iris_summary.txt", "w") as f:
@@ -40,53 +39,91 @@ print("\n-----No duplicates as shown-----")
 iris_data[iris_data.duplicated()]
 
 # count the exact number of species
+# Reference - https://www.tutorialspoint.com/how-can-i-show-figures-separately-in-matplotlib
+# Reference to close figures for the data to show only within the named function -
+# https://matplotlib.org/2.0.2/api/pyplot_api.html
+
 print("\n-----Count for the number of species-----")
 print(iris_data.value_counts("species"))
 
 # histogram for sepal length data
 print("\n-----Sepal Length Histogram-----")
 
-plt.figure(figsize = (11,8))
-x = iris_data["sepal_length"]
-
-plt.hist(x, bins = 20, color = "blue")
-plt.title("Sepal Length in cm")
-plt.xlabel("Sepal_Length_in_cm")
-plt.ylabel("Count")
-plt.savefig("sepal_length.png")
+def sepal_length_histo_func():
+    plt.figure(figsize = (11,8))
+    x = iris_data["sepal_length"]
+    plt.hist(x, bins = 20, color = "blue")
+    plt.title("Sepal Length in cm")
+    plt.xlabel("Sepal_Length_in_cm")
+    plt.ylabel("Count")
+    plt.savefig("sepal_length.png")
+    plt.close()
+sepal_length_histo_func()
 
 # histogram for sepal width data
 print("\n-----Sepal Width Histogram-----")
 
-plt.figure(figsize = (11,8))
-x = iris_data["sepal_width"]
-
-plt.hist(x, bins = 20, color = "blue")
-plt.title("Sepal Width")
-plt.xlabel("Sepal_Width_in_cm")
-plt.ylabel("Count")
-plt.savefig("sepal_width.png")
+def sepal_width_histo_func():
+    plt.figure(figsize = (11,8))
+    x = iris_data["sepal_width"]
+    plt.hist(x, bins = 20, color = "blue")
+    plt.title("Sepal Width in cm")
+    plt.xlabel("Sepal_Width_in_cm")
+    plt.ylabel("Count")
+    plt.savefig("sepal_width.png")
+    plt.close()
+sepal_width_histo_func()
 
 # histogram for petal length data
 print("\n-----Petal Length Histogram-----")
 
-plt.figure(figsize = (11,8))
-x = iris_data["petal_length"]
-
-plt.hist(x, bins = 20, color = "green")
-plt.title("Petal Length")
-plt.xlabel("Petal_Length_in_cm")
-plt.ylabel("Count")
-plt.savefig("petal_length.png")
+def petal_length_histo_func():
+    plt.figure(figsize = (11,8))
+    x = iris_data["petal_length"]
+    plt.hist(x, bins = 20, color = "green")
+    plt.title("Petal Length in cm")
+    plt.xlabel("Petal_Length_in_cm")
+    plt.ylabel("Count")
+    plt.savefig("petal_length.png")
+    plt.close()
+petal_length_histo_func()
 
 # histogram for petal width data
 print("\n-----Petal Width Histogram-----")
 
-plt.figure(figsize = (11,8))
-x = iris_data["petal_width"]
+def petal_width_histo_func():
+    plt.figure(figsize = (11,8))
+    x = iris_data["petal_width"]
+    plt.hist(x, bins = 20, color = "green")
+    plt.title("Petal Width in cm")
+    plt.xlabel("Petal_Width_in_cm")
+    plt.ylabel("Count")
+    plt.savefig("petal_width.png")
+    plt.close()
+petal_width_histo_func()
 
-plt.hist(x, bins = 20, color = "green")
-plt.title("Petal Width")
-plt.xlabel("Petal_Width_in_cm")
-plt.ylabel("Count")
-plt.savefig("petal_width.png")
+# scatter plots
+# Reference - https://pandas.pydata.org/pandas-docs/dev/reference/api/pandas.DataFrame.plot.scatter.html
+print("\n-----Scatter plot for Sepal Length vs Sepal Width-----")
+
+def sepal_scatter_func():
+    plt.scatter(iris_data["sepal_length"], iris_data["sepal_width"])
+    plt.title("Sepal Length vs Sepal Width", loc ="left", fontsize=15)
+    plt.xlabel("sepal_width")
+    plt.ylabel("sepal_length")
+    plt.savefig("Sepal_Scatter_plot.png")
+    plt.close()
+sepal_scatter_func()
+
+print("\n-----Scatter plot for Petal Length vs Petal Width-----")
+
+def petal_scatter_func():
+    plt.scatter(iris_data["petal_length"], iris_data["petal_width"])
+    plt.title("Petal Length vs Petal Width", loc ="left", fontsize=15)
+    plt.xlabel("petal_width")
+    plt.ylabel("petal_length")
+    plt.savefig("Petal_Scatter_plot.png")
+    plt.close()
+petal_scatter_func()
+
+
